2956f5b400f7e03d794bf62c6951fcb74c43c112
Divesh Otwani <divesh.otwani@gmail.com>
2020-04-01 08:22:58 +0200

    work with linear types

--- src/GHC/TcPluginM/Extra.hs
+++ src/GHC/TcPluginM/Extra.hs
@@ -390,7 +390,7 @@ substType _subst t@(ForAllTy _tv _ty) =
   -- ForAllTy tv (substType subst ty)
   t
 #if __GLASGOW_HASKELL__ >= 809
-substType subst (FunTy af t1 t2) =
+substType subst (FunTy _ af t1 t2) =
   FunTy af (substType subst t1) (substType subst t2)
 #elif __GLASGOW_HASKELL__ >= 802
 substType subst (FunTy t1 t2) =
